de4b0c4a6b0625775fd936042de4b071e42666a7
Genesis-ctrl-X <mahirchopra555@gmail.com>
2026-05-18 01:41:46 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    unordered_map<int,int>hash;
+    for(int num:nums){
+        hash[num]++;
+      }
+      for(int num:nums){
+        if(hash[num]>1) return num;
+      }
+      return -1;
+    }
+
+};
