52da8ffd00d458e046579cddae114e6b8b6df5e1
mahirc24 <mahirchopra555@gmail.com>
2026-05-27 05:00:52 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    vector<int>ways(n+1);
+    if(n<=2)return n;
+    ways[1]=1;
+    ways[2]=2;
+    for(int i=3;i<n+1;i++){
+        ways[i]=ways[i-1]+ways[i-2];
+    }
+    return ways[n];
+    }
+};
