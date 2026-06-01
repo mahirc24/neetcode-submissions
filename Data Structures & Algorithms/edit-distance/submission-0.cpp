db26d51a1327fdcf39fd5237fe9dc2f5474c8dd2
mahirc24 <mahirchopra555@gmail.com>
2026-06-02 04:56:34 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+    string s1=word1;
+    string s2=word2;
+    int n = word1.size(); int m = word2.size();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    return f(n-1,m-1,s1,s2,dp);
+    }
+    int f(int i,int j, string &s1, string &s2,vector<vector<int>>&dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j] = f(i-1,j-1,s1,s2,dp);
+        }
+        else{
+            return dp[i][j] = min({1+f(i-1,j-1,s1,s2,dp),1+f(i-1,j,s1,s2,dp),1+f(i,j-1,s1,s2,dp )});
+        }
+    }
+};
