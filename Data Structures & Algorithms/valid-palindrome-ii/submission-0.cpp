1d3e1cc3f47fb5f74844cc7abad7e0daa579eead
mahirc24 <mahirchopra555@gmail.com>
2026-06-19 17:13:32 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    bool isPalindrome(string &s){
+        string str = s;
+        reverse(s.begin(),s.end());
+        if(str==s) return true;
+        else return false;
+    }
+public:
+    bool validPalindrome(string s) {
+        if(isPalindrome(s)==true) return true;
+        else{
+            for(int i=0;i<s.size();i++){
+                string str = s;
+                str.erase(i,1);
+                if(isPalindrome(str)==true)return true;
+            }
+        }
+        return false;
+    }
+
+};
